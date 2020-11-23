a3fa5214dc4bf80103beacb6ad882fd3c127a277
Trung Tin <tincao241@gmail.com>
2020-11-23 20:44:40 +0700

    nothing change

--- public/test.php
+++ public/test.php
@@ -1,12 +1,5 @@
 <?php 
 
     require_once __DIR__. "/../autoload/autoload.php";
-    $data_user = $db -> fetchOne('user',"email = 'ssadasdtudent@gmail.com'"); 
-    var_dump($data_user);
-    if(is_array($data_user)){
-        echo "yes";
-    }
-    else{
-        echo "not";
-    }
+    
 ?>
