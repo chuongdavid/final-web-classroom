0721d2fdb0a74a5090d95e448c04e24ba7993a5a
Trung Tin <tincao241@gmail.com>
2020-11-25 11:33:56 +0700

    Test announcement

--- public/index-giaovien.php
+++ public/index-giaovien.php
@@ -202,49 +202,49 @@ if(isset($_POST['class_name']) && isset($_POST['class_subject']) && isset($_POST
                 </div>
     <!--classes-->
     <div class=" index container"> 
-    <div class="class-place">
-        <div class="row class-container">
-            <?php foreach ($class as $item):?>
-            <!-- each class -->
-            <a href="stream.php?id=<?php echo $item['id']?>">
-            <div class="classcard col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="cell">
+        <div class="class-place">
+            <div class="row class-container">
+                <?php foreach ($class as $item):?>
+                <!-- each class -->
+                <a href="stream.php?id=<?php echo $item['id']?>">
+                    <div class="classcard col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                        <div class="cell">
 
-                    <div class="class-inf">
-                    <div>
-                        <h1 class="class-title text-left ml-3 mb-1"> <?php echo $item['name'] ?> </h1>
-                    </div>
-                        <div class="text-left ml-3 mt-0"><img src="<?php echo base_url() ?>/public/uploads/class/<?php echo $item['image'] ?>" class="avatar" style="width:13%; border-radius: 50%"> <?php $item['teacher'] ?><?php echo $item['teacher'] ?></div>
-                        <a href="edit-class.php?id=<?php echo $item['id']?>"><i class="editclassroom fas fa-pen text-dark"></i></a>
-                        <a href="delete-class.php?id=<?php echo $item['id']?>" onclick="return confirm('Are you sure to delete this class !'); "><i class="editclassroom far fa-trash-alt text-dark"></i></a>
-                    </div>
-
-                    <div class="class-main p-2">
-                        <p class="title"> <?php echo $item['subject'] ?></p>
-                    </div>
-
-                    <div class="class-footer ">
-                        <span class="circle">
-                            <div class="work-icon">
-                                <i class="fa fa-user-o " aria-hidden="true"></i>
+                            <div class="class-inf">
+                                <div>
+                                    <h1 class="class-title text-left ml-3 mb-1"> <?php echo $item['name'] ?> </h1>
+                                </div>
+                                    <div class="text-left ml-3 mt-0"><img src="<?php echo base_url() ?>/public/uploads/class/<?php echo $item['image'] ?>" class="avatar" style="width:13%; border-radius: 50%"> <?php $item['teacher'] ?><?php echo $item['teacher'] ?></div>
+                                    <a href="edit-class.php?id=<?php echo $item['id']?>"><i class="editclassroom fas fa-pen text-dark"></i></a>
+                                    <a href="delete-class.php?id=<?php echo $item['id']?>" onclick="return confirm('Are you sure to delete this class !'); "><i class="editclassroom far fa-trash-alt text-dark"></i></a>
                             </div>
-                        </span>
 
-                        <span class="circle">
-                            <div class="folder-icon">
-                                <i class="fa fa-folder-o" aria-hidden="true"></i>
+                            <div class="class-main p-2">
+                                <p class="title"> <?php echo $item['subject'] ?></p>
                             </div>
-                        </span>
+
+                            <div class="class-footer ">
+                                <span class="circle">
+                                    <div class="work-icon">
+                                        <i class="fa fa-user-o " aria-hidden="true"></i>
+                                    </div>
+                                </span>
+
+                                <span class="circle">
+                                    <div class="folder-icon">
+                                        <i class="fa fa-folder-o" aria-hidden="true"></i>
+                                    </div>
+                                </span>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </a>
+                <?php endforeach ?>
+
+                <!-- end each class -->
             </div>
-            </a>
-            <?php endforeach ?>
-
-            <!-- end each class -->
-            
         </div>
-        </div> 
+        
             <!-- div class class-place -->
     </div>
 
