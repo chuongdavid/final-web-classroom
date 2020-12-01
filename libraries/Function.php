a5ed8608e7bedf2e7d530f4b261e60f1550e93dc
ChuongDuong <chuongddavid@gmail.com>
2020-12-01 20:22:23 +0700

    Ket thuc

--- libraries/Function.php
+++ libraries/Function.php
@@ -242,6 +242,44 @@ function send_reset_password_email($email,$token){
         return false;
     }
 }
+function send_notification($teacher_name,$class_name,$email){
+    $mail = new PHPMailer(true);
+    try{
+        //Server settings
+        $mail->CharSet = 'utf-8';                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'chuongddavid@gmail.com';                     // SMTP username
+        $mail->Password   = 'ieibumydddhemnei';                               // SMTP password
+        $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('chuongddavid@gmail.com', 'Classroom');
+        $mail->addAddress($email, 'Receiver');     // Add a recipient
+        // $mail->addAddress('ellen@example.com');               // Name is optional
+        // $mail->addReplyTo('info@example.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+
+        // // Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'You have a notification from your class';
+        $mail->Body    = "<p>Teacher: $teacher_name have just posted an announcement in class: $class_name. <a href='".base_url()."/public/login.php'>Login</a> to check it out</p>";
+        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        return true;
+    }
+    catch (Exception $e) {
+        return false;
+    }
+}
 function check_role($email){
     $db = new Database;
     $data_user =  $db -> fetchOne('user',"email = '".$email."'");
